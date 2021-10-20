3ab6d9fa25093c43e44d0cfecddade4a975078b0
KelompokFYA <mahyudinyudi01@gmail.com>
2021-10-20 21:43:15 +0700

    Initial commit

---
+++ app/src/main/java/com/klpbandrek/musicplaylist/networking/Api.kt
@@ -0,0 +1,6 @@
+package com.klpbandrek.musicplaylist.networking
+
+object Api {
+    var ListMusic = "https://animemusic.us/listmusic"
+    var DetailMusic = "https://animemusic.us/jsongetid/{id}"
+}
